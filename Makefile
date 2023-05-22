8589d79fa2bcd2157d59ed10b3474ca83303f454
sebamiro <seba.mi.21@gmail.com>
2023-05-22 13:54:50 -0400

    Funciona

--- Makefile
+++ Makefile
@@ -9,10 +9,12 @@ down:
 	@docker compose -f ./src/docker-compose.yml down
 
 clean:
+	@rm -rf /home/smiro/data/mysql/*
+	@rm -rf /home/smiro/data/wordpress/*
 	@docker stop $$(docker ps -qa)
 	@docker rm $$(docker ps -qa)
 	@docker rmi $$(docker images -qa)
 	@docker volume rm $$(docker volume ls -q)
-	@docker network rm $$(docker network ls -q)
+	@docker network rm smironet
 
 .PHONY: all down clean
--- src/requirements/mariadb/tools/mariadb.sh
+++ src/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,9 @@
-#//////////////////////////////////////////////////////////////////////#
-#	_mariadb.sh                                                        #
-#//////////////////////////////////////////////////////////////////////#
-
+#! /bin/bash
 service mysql start
 mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
-mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFY BY '$MYSQL_PASSWORD'"
-mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFY BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -u root -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysqladmin -u root password "$MYSQL_ROOT_PASSWORD"
 
-exec "$@"
+mysqld
